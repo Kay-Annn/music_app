6741f8d13bd349c87927ca5f2600b6d2479d5b13
Kay <ckay_006@yahoo.com>
2023-03-28 10:51:01 -0400

    google auth added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Login, Home } from './components';
 
+
 const App = () => {
+const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true")
+
   return (
     <div className='w-screen h-screen bg-primary flex justify-center items-center'>
       <Routes>
